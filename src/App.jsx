6e5076a1c0887498780be0f208158ba1614ee936
Gathan Mahesa <gthn217@gmail.com>
2023-06-08 12:16:05 +0700

    Created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
 /* eslint-disable react/prop-types */
 import { useState } from 'react';
-import './App.css';
-
+import Logo from './assets/Logo.png';
 import bgImage from './assets/AmericanManhunt.jpg';
 import titleImage from './assets/MainTitle.png';
 import Episode1 from './assets/Eps1.png';
 import Episode2 from './assets/AmericanManhunt.jpg';
 import Episode3 from './assets/Eps3.png';
-
-const Navbar = () => {
-	return (
-		<div id="navbar" className='fixed text-white pt-5'>
-			<div className='flex w-screen gap-40'>
-				<div id = "left" className="max-w-2xl w-full"> </div>
-				<div id = "right" className="">
-					<ul className='flex gap-10'>
-						<li>Series</li>
-						<li>Movies</li>
-						<li>Genre</li>
-						<li>Year</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	)
-}
 
 const HeroContent = () => {
   const briefDescription =
@@ -32,7 +13,10 @@ const HeroContent = () => {
   const information = [2023, '1 Season', 'Documentaries', '16+'];
 
   return (
-    <div id="hero-content" className="text-white max-w-2xl h-screen flex flex-col pt-36 px-24 gap-10">
+    <div id="hero-content" className="relative text-white max-w-2xl h-screen flex flex-col justify-center px-24 gap-10">
+      <div className="absolute top-10 w-24 flex gap-6">
+        <img src={Logo} alt="" />
+      </div>
       <img src={titleImage} alt="main-title" className="select-none" draggable="false" />
       <h1 className="gotham text-xl">{briefDescription}</h1>
       <ul>
@@ -62,17 +46,44 @@ const Episode = ({ episodeNumber, title, image }) => (
 
 const Episodes = () => (
   <div id="episodes" className="text-white flex flex-row flex-nowrap overflow-x-hidden items-center gap-8">
+    <div id="navbar" className="w-full fixed max-w-2xl text-white top-10">
+      <ul className="flex gap-10">
+        <li>
+          <a>Movies</a>
+        </li>
+        <li>
+          <a>Series</a>
+        </li>
+        <li>
+          <a>Genre</a>
+        </li>
+        <li>
+          <a>Year</a>
+        </li>
+        <li>
+          <div className="absolute right-28 hover:cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+              <style>{`svg{fill:#ffffff}`}</style>
+              <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+            </svg>
+          </div>
+        </li>
+      </ul>
+    </div>
     <Episode episodeNumber={1} title="White Hat, Black Hat" image={Episode1} />
     <Episode episodeNumber={2} title="The American Dreams" image={Episode2} />
     <Episode episodeNumber={3} title="You Can't Interview A Corpse" image={Episode3} />
   </div>
-
 );
 
+const LeftSide = () => <HeroContent />;
+
+const RightSide = () => <Episodes />;
+
 const Content = () => (
-  <div id="content" className="absolute w-full h-screen flex gap-40">
-    <HeroContent />
-    <Episodes />
+  <div id="content" className="absolute w-full h-screen flex justify-center gap-20">
+    <LeftSide />
+    <RightSide />
   </div>
 );
 
@@ -88,11 +99,10 @@ const BackgroundImage = () => {
 
 function App() {
   return (
-    <>
-			<Navbar/>
+    <div className="flex justify-center">
       <Content />
       <BackgroundImage />
-    </>
+    </div>
   );
 }
 
