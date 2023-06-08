e6d50569119a80e9aefc5e153385499ad9220a23
Gathan Mahesa <gthn217@gmail.com>
2023-06-08 10:59:13 +0700

    Add navbar, change episode size

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,24 @@ import Episode1 from './assets/Eps1.png';
 import Episode2 from './assets/AmericanManhunt.jpg';
 import Episode3 from './assets/Eps3.png';
 
+const Navbar = () => {
+	return (
+		<div id="navbar" className='fixed text-white pt-5'>
+			<div className='flex w-screen gap-40'>
+				<div id = "left" className="max-w-2xl w-full"> </div>
+				<div id = "right" className="">
+					<ul className='flex gap-10'>
+						<li>Series</li>
+						<li>Movies</li>
+						<li>Genre</li>
+						<li>Year</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	)
+}
+
 const HeroContent = () => {
   const briefDescription =
     'Uncover the gripping truth of American Manhunt: The Boston Marathon Bombing, a Netflix documentary that reveals the intense investigation and manhunt for justice after the tragic 2013 event.';
@@ -25,33 +43,34 @@ const HeroContent = () => {
         ))}
       </ul>
       <div className="w-36 h-10">
-        <button className="bg-red-600 border-red-600 w-full h-full rounded-lg">Watch Trailer</button>
+        <button className="bg-red-600 border-red-600 w-full h-full rounded-lg">Watch Now</button>
       </div>
     </div>
   );
 };
 
 const Episode = ({ episodeNumber, title, image }) => (
-  <div className="relative w-96 h-60 flex-shrink-0">
-    <div id="Information" className="absolute flex flex-col bottom-5 left-2 gap-2">
+  <div className="relative w-60 h-96 flex-shrink-0">
+    <div id="Information" className="absolute flex flex-col bottom-5 left-4 gap-2">
       <h2 className="text-gray-300 z-10">{`Episode ${episodeNumber}`}</h2>
       <h1 className="text-white z-10">{title}</h1>
     </div>
-    <img src={image} alt={`Episode ${episodeNumber}`} className="w-full h-full object-cover" />
-    <div className="dark-overlay"></div>
+    <img src={image} alt={`Episode ${episodeNumber}`} className="w-full h-full object-cover rounded-xl" />
+    <div className="dark-overlay rounded-xl"></div>
   </div>
 );
 
 const Episodes = () => (
-  <div id="episodes" className="text-white flex flex-row flex-nowrap overflow-x-hidden items-center gap-10">
+  <div id="episodes" className="text-white flex flex-row flex-nowrap overflow-x-hidden items-center gap-8">
     <Episode episodeNumber={1} title="White Hat, Black Hat" image={Episode1} />
     <Episode episodeNumber={2} title="The American Dreams" image={Episode2} />
     <Episode episodeNumber={3} title="You Can't Interview A Corpse" image={Episode3} />
   </div>
+
 );
 
 const Content = () => (
-  <div id="content" className="absolute w-full h-screen flex">
+  <div id="content" className="absolute w-full h-screen flex gap-40">
     <HeroContent />
     <Episodes />
   </div>
@@ -59,7 +78,6 @@ const Content = () => (
 
 const BackgroundImage = () => {
   const [isOverlayVisible, setIsOverlayVisible] = useState(false);
-
   return (
     <div id="bg-image" className="absolute w-full h-screen z-[-1]">
       {isOverlayVisible && <div id="overlay" className="fixed bg-black opacity-50 w-full h-full"></div>}
@@ -71,6 +89,7 @@ const BackgroundImage = () => {
 function App() {
   return (
     <>
+			<Navbar/>
       <Content />
       <BackgroundImage />
     </>
