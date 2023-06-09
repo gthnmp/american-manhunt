d8206e9bc5b1123c8b6566f881805192e2dcaf48
Gathan Mahesa <gthn217@gmail.com>
2023-06-09 10:27:07 +0700

    Finishedm just need to add smoothscrolling

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 // import { gsap } from 'gsap';
 
-import Logo from './assets/Logo.png';
+import Logo from '/react.svg';
 import titleImage from './assets/MainTitle.png';
 import bgImage from './assets/Eps2.png';
 import Episode1 from './assets/Eps1.png';
@@ -13,12 +13,16 @@ import Trailer1 from './assets/Trailer1.jpg'
 import Trailer2 from './assets/Trailer2.jpg'
 import Star from './assets/star.png';
 
-
-const Episode = ({ episodeTitle, title, image }) => (
+const Episode = ({ episodeTitle, title, image, duration = 0 }) => (
 	<div className="relative select-none">
+
 		<div id="Information" className="absolute flex flex-col bottom-5 left-4 gap-2">
 			<h2 className="text-gray-300 z-10">{`${episodeTitle}`}</h2>
 			<h1 className="text-white z-10">{title}</h1>
+		</div>
+		
+		<div id="duration" className='absolute bottom-5 right-4 z-10'>
+			{duration != 0 && <h1 className='text-white'>{duration} m</h1>}
 		</div>
 
 		<div className='relative w-96 h-60'>
@@ -29,6 +33,23 @@ const Episode = ({ episodeTitle, title, image }) => (
 	</div>
 );
 
+const Navbar = () => (
+	<header className='fixed py-8 px-16 w-screen z-50 flex items-center justify-between'>
+
+		<div className='flex items-center gap-5'>
+			<img src={Logo} alt="" />
+			<h1 className='text-white text-xl gotham font-bold'>Jue Viole Grace</h1>
+		</div>
+		
+		<ul className='text-white flex flex-row gap-16'>
+			<li><a>Movies</a></li>
+			<li><a>Series</a></li>
+			<li><a>Genres</a></li>
+			<li><a>Year</a></li>
+		</ul>
+	</header>
+)
+
 const Introduction = () => {
   const briefDescription =
     'Uncover the gripping truth of American Manhunt: The Boston Marathon Bombing, a Netflix documentary that reveals the intense investigation and manhunt for justice after the tragic 2013 event.';
@@ -36,10 +57,6 @@ const Introduction = () => {
 
   return (
     <div id="hero-content" className="text-white max-w-xl h-screen">
-
-      <div className="absolute top-10 w-24 left-16">
-        <img src={Logo} alt="" />
-      </div>
 
 			<div id = "main-content" className='gap-10 h-screen flex flex-col justify-center'>
 				<img src={titleImage} alt="main-title" className="select-none" draggable="false" />
@@ -81,13 +98,47 @@ const Videos = () => (
 		</div>
 
 		<div className='flex justify-center gap-10'>
-			<Episode episodeTitle={"Episode 1"} title={"White Hat, Black Hat"} image={Episode1}/>
-			<Episode episodeTitle={"Episode 2"} title={"The American Dream"} image={Episode2}/>
-			<Episode episodeTitle={"Episode 3"} title={"You Can't Interview A Corpse"} image={Episode3}/>
+			<div className='flex flex-col gap-5'>
+				<Episode episodeTitle={"Episode 1"} title={"White Hat, Black Hat"} image={Episode1} duration={62}/>
+				<p className='text-gray-400 text-sm'>In the hours after the blasts, investigators pore over evidence. One photo and a few seconds of footage lead to a breakthrough – and an agonizing choice.</p>
+			</div>
+			<div className='flex flex-col gap-5'>
+				<Episode episodeTitle={"Episode 2"} title={"The American Dream"} image={Episode2} duration={53}/>
+				<p className='text-gray-400 text-sm'>The brothers’ images are released to the public, setting off a violent chain of events. Meanwhile, clues from their past tell a complicated story.</p>
+			</div>
+			<div className='flex flex-col gap-5'>
+				<Episode episodeTitle={"Episode 3"} title={"You Can't Interview A Corpse"} image={Episode3} duration={63}/>
+				<p className='text-gray-400 text-sm'>Dzhokhar Tsarnaev is cornered into an intense standoff. As officials navigate remaining questions and suspicions, the city emerges – Boston Strong.</p>
+			</div>
+		</div>
+
+		<div className='flex justify-center gap-10 text-2xl'>
+			<button>{"<"}</button>
+			<button>{">"}</button>
 		</div>
 		
 	</div>
 );
+
+const Details = () => (
+	<div>
+		<div className='flex flex-col gap-4'>
+			<h1 className='text-4xl gotham font-bold'>More Details</h1>
+			<div>
+				<h2 className='text-2xl gotham font-normal'>Watch Offline</h2>
+				<p className='text-gray-400 text-base'>Available To Download</p>
+			</div>
+			<div>
+				<h2 className='text-2xl gotham font-normal'>Genres</h2>
+				<p className='text-gray-400 text-base'>Historical Documentaries, Crime TV Shows, Docuseries, US TV Shows, True Crime Documentaries</p>
+			</div>
+			<div>
+				<h2 className='text-2xl gotham font-normal'>More</h2>
+				<p className='text-gray-400 text-base'>Go behind the scenes and learn more on Tudum.com</p>
+			</div>
+		</div>
+	</div>
+)
 
 const Footer = () => (
 	<footer className='bg-neutral-950 text-gray-400 flex justify-center h-96 border-t-2 border-neutral-500 my-20 py-20'>
@@ -107,14 +158,16 @@ const Footer = () => (
 	</footer>
 )
 
-const Content = () => (
-  <div id="content" className="absolute w-full h-screen flex flex-col gap-5 px-16 text-white">
-    <Introduction />
-		<Videos/>
-		<Footer/>
-  </div>
-);
-
+const Content = () => {
+	return(
+		<div id="content" className="absolute w-full h-screen flex flex-col gap-5 px-16 text-white">
+			<Introduction />
+			<Videos/>
+			<Details/>
+			<Footer/>
+		</div>
+	);
+}
 const BackgroundImage = () => {
   const [isOverlayVisible, setIsOverlayVisible] = useState(false);
 
@@ -129,6 +182,7 @@ const BackgroundImage = () => {
 function App() {
   return (
     <div className="flex justify-center">
+			<Navbar/>
       <Content />
       <BackgroundImage />
     </div>
